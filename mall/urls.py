0e91366352a4d6b551c203388d4bbf2b643a4d25
morganehakoune <mohakoune@gmail.com>
2021-05-20 19:00:21 +0300

    update creating new_shop

---
+++ mall/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from store_owner import views as store_owner_views
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('dashboard/', store_owner_views.dashboard, name = 'dashboard'),
+    path('new_product/', store_owner_views.new_product, name='new_product'),
+    path('update_product/', store_owner_views.update_product, name='update_product'),
+    path('my_products/', store_owner_views.my_products, name='my_product'),
+    path('status/', store_owner_views.status, name='status'),
+    path('new_shop/', store_owner_views.new_shop, name='new_shop'),
+    path('customers/', store_owner_views.customers, name='customers'),
+    path('create_product/', store_owner_views.createProduct, name='create_product'),
+
+]
