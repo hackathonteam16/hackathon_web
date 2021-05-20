0c29445b983964078205547c2827f3ed1f181a1e
morganehakoune <mohakoune@gmail.com>
2021-05-20 13:56:34 +0300

    update creating new_shop

--- store_owner/admin.py
+++ store_owner/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
+admin.site.register(Shop)
+
 
 # Register your models here.
--- store_owner/models.py
+++ store_owner/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Shop(models.Model):
+    shop_name = models.CharField(max_length=200, null=True)
+    description= models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.shop_name
