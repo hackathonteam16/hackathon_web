bb79a3f828e66e4ef11b20f790a4f2fc2f8502e5
morganehakoune <mohakoune@gmail.com>
2021-05-20 23:05:22 +0300

    update creating new_shop

--- store_owner/forms.py
+++ store_owner/forms.py
@@ -1,6 +1,10 @@
 from django.forms import ModelForm
 from .models import Product
+from django import forms
+
 class OrderForm(ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    price = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
     class Meta:
         model = Product
         fields = ['name','price','category','description','size']
